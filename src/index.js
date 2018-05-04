e3e75e5023230a13a14a1c55bac4de5f63e9e185
cjshinning <784319417@qq.com>
2018-05-04 11:28:52 +0800

    react是什么

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// 新建reducer，描述action如何把一个state转变成下一个state
+function counter(state,action){
+    switch(action.type){
+        case 'INC':
+            return state+1
+        case 'DEC':
+            return state-1
+        default:
+            return 10
+    }
+}
+
+// 创建store来存储状态
+let store = createStore(counter)
+
+// 手动订阅
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+// 派发事件，传递action
+// 改变state的唯一方法是dispatch一个action
+store.dispatch({type: 'INC'})
+store.dispatch({type: 'INC'})
+store.dispatch({type: 'DEC'})
+store.dispatch({type: 'INC'})
